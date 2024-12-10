eb766d12ce96cc10b61309dfd8ed395cb4650a3d
athena272 <guilhermera272@gmail.com>
2024-12-10 14:31:02 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Routes from './Routes/index.tsx'
 import GlobalStyle from './styles/global.ts'
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
-
+      <Routes />
     </>
   )
 }
