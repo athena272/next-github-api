0c85f57a8fe1dcde5c4766d0df991a46822bbc5f
athena272 <guilhermera272@gmail.com>
2024-12-12 18:07:43 -0300

    Update index.tsx

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,47 @@
+import { useCallback, useState } from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from "./styles";
+import api from '../../services/api';
+
+type Repository = {
+    name: string;
+};
 
 export default function Main() {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState<Repository[]>([]);
+
+    const handleSubmit = useCallback(
+        (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault()
+
+            async function submit() {
+                try {
+                    const response = await api.get(`repos/${newRepo}`)
+
+                    const data: Repository = {
+                        name: response.data.full_name,
+                    };
+
+                    setRepositorios([...repositorios, data])
+                    setNewRepo('')
+                } catch (error) {
+                    console.error("Erro ao buscar o repositório:", error)
+                }
+            }
+
+            submit()
+        },
+        [newRepo, repositorios]
+    )
+
+    const handleInputChange = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            setNewRepo(event.target.value)
+        },
+        []
+    );
+
     return (
         <Container>
             <h1>
@@ -9,8 +49,13 @@ export default function Main() {
                 Meus repositorios
             </h1>
 
-            <Form>
-                <input type="text" placeholder="Adicionar repositorios" />
+            <Form onSubmit={(event) => handleSubmit(event)}>
+                <input
+                    type="text"
+                    placeholder="Adicionar repositorios"
+                    value={newRepo}
+                    onChange={(event) => handleInputChange(event)}
+                />
 
                 <SubmitButton>
                     <FaPlus color="#fff" size={14} />
