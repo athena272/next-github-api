37a76f0109cd1e9fa1a124a5a076cc8690da475a
athena272 <guilhermera272@gmail.com>
2024-12-11 23:42:07 -0300

    Update index.tsx

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -6,8 +6,16 @@ export default function Main() {
         <Container>
             <h1>
                 <FaGithub size={25} />
-                Meus Repositorios
+                Meus repositorios
             </h1>
+
+            <Form>
+                <input type="text" placeholder="Adicionar repositorios" />
+
+                <SubmitButton>
+                    <FaPlus color="#fff" size={14} />
+                </SubmitButton>
+            </Form>
         </Container>
     )
 }
